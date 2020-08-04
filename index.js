b0d059271b18f55d2ed1bf0e3ae8745b41f33d3d
svidovich <samuel.vidovich@gmail.com>
2020-08-03 21:16:03 -0400

    Works on Linux. FIXME: Files with spaces in them??? Basic styling done.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const electron = require('electron');
 const dialog = electron.remote.dialog;
+const path = require('path');
 
 // Required to convert the media
-const ffmpeg = require('ffmpeg');
+const ffmpeg = require('js-ffmpeg');
 
 let uploadFile = document.getElementById('upload-file');
 let filePath = undefined;
@@ -23,13 +24,15 @@ uploadFile.addEventListener('click', () => {
                 // Get the filepath, and split it a couple of different ways
                 // in case we need the pieces.
                 filePath = file.filePaths[0].toString();
-                let fileName = filePath.split('/').slice(-1)[0];
+                let fileName = path.basename(filePath);
+
                 let fileNameNoExtension = fileName.split('.')[0];
                 let postUploadSpan = document.getElementById('after-file-upload');
 
                 // Show the user what file they selected in the UI.
                 let conversionTypesDropdown = document.getElementById('conversion-types-dropdown');
-                postUploadSpan.innerText = `File selected: ${filePath}
+                postUploadSpan.innerText = `File selected: ${filePath}!
+
                 Select output type:`;
 
                 // Make output type selection visible. Add a listener, and wait for change.
@@ -41,13 +44,21 @@ uploadFile.addEventListener('click', () => {
                 document.getElementById('conversion-types-dropdown').addEventListener('change', selectedTypeEvent => {
                     selectedTypeValue = document.getElementById('conversion-types-dropdown').value;
                     userNotifyTypeSelected = document.getElementById('after-type-selected');
+                    // Show user the type that's currently selected. This is probably just junk.
                     userNotifyTypeSelected.innerText = "\nType Selected: " + selectedTypeValue;
                     userNotifyTypeSelected.style.visibility = 'visible';
                 })
+
+                // Show the convert button.
                 let convertButtonSpan = document.getElementById('convert-button-span');
                 let convertButton = document.getElementById('convert-button');
                 convertButtonSpan.style.visibility = 'visible';
-                convertButton.addEventListener('click', () => {
+
+                // Wait for usr to click the convert button. When they do,
+                // show the save file dialog.
+                // Use onclick instead of adding an event listener to prevent goofy bugs.
+
+                convertButton.onclick = () => {
                     dialog.showSaveDialog({
                         title: 'Save Converted File',
                         defaultPath: __dirname + "/" + fileNameNoExtension + '.' + selectedTypeValue,
@@ -56,23 +67,40 @@ uploadFile.addEventListener('click', () => {
                             'showOverwriteConfirmation'
                         ]
                     }).then(saveFile => {
+                        convertButton.removeEventListener('click', () => { });
                         if (!saveFile.canceled) {
+                            // Get information about the output filename and show user.
                             saveFilePath = saveFile.filePath.toString();
+                            saveFileDirectory = path.dirname(saveFilePath);
+                            saveFileName = path.basename(saveFilePath);
+                            // Pad the save file name for use with ffmpeg
+                            saveFileNamePadded = `"${saveFileName}"`;
+                            paddedSaveFilePath = saveFileDirectory + "/" + saveFileNamePadded;
+
                             confirmation = document.getElementById('confirmation-span');
-                            confirmation.innerText = "Saved converted file to " + saveFilePath + "!";
                             confirmation.style.visibility = 'visible';
+                            // Run ffmpeg. Do _not_ infer the output type from the save data. Use the info
+                            // from the dropdown selection.
+                            // TODO: Apparently doesn't support files with spaces in them, which is
+                            // problematic? Very strange -- I can't seem to get it to work, no matter how
+                            // I pad the strings.
+                            ffmpeg.ffmpeg(filePath, [`-f ${selectedTypeValue}`], saveFilePath, progress => {
+                                console.log(progress);
+                            }).success(() => {
+                                confirmation.innerText = "Saved converted file to " + saveFilePath + "!";
+                            }
+                            ).error(error => {
+                                confirmation.innerText = "Failed to convert file. Here's an error message: \n" + error.message;
+                            })
                         }
                     })
-                })
-
-
-
+                }
             }
             // If there was an error, console.log that shit
         }).catch(err => {
             console.log(err);
         })
     } else {
-        document.write('Not yet supported. Sorry about your luck. File a github issue.')
+        document.write('Mac not yet supported. Sorry about your luck. File a github issue.')
     }
 })
